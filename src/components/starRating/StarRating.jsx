df57a9bbfe28b14d44453cdfb6c9da84534d8527
gunjan <sengargunjan26@gmail.com>
2024-11-22 10:47:52 +0530

    Star rating fully completed

--- src/components/starRating/StarRating.jsx
+++ src/components/starRating/StarRating.jsx
@@ -3,37 +3,36 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
 function StarRating() {
-  const [checkedValue, setCheckedValue] = useState(0);
+  const [checked, setChecked] = useState(0);
 
   const handleChange = (value) => {
-    setCheckedValue(checkedValue === value ? null : value); // Toggle between selected or unselected
+    setChecked(value === checked ? null : value);
   };
-
   return (
-    <div className="bg-yellow-400 w-[100vw] h-[100vh] flex justify-center items-center">
-      <div className="w-[40%] h-[40%] bg-black flex justify-center items-center text-5xl">
-        {[1, 2, 3, 4, 5].map((value) => (
-          <div key={value}>
-            <input
-              type="radio"
-              className="hidden peer cursor-pointer"
-              id={`star-${value}`}
-              checked={checkedValue === value}
-              onChange={() => handleChange(value)}
-            />
-
-            {/* Label linked to the radio input */}
-            <label
-              htmlFor={`star-${value}`}
-              className={`text-gray-400 ${
-                checkedValue >= value ? "text-yellow-500" : ""
-              } transition-all duration-300`}
-            >
-              {/* FontAwesome icon */}
-              <FontAwesomeIcon icon={faStar} />
-            </label>
-          </div>
-        ))}
+    <div className="w-[100vw] h-[100vh] bg-yellow-400 flex justify-center items-center">
+      <div className="w-[40vw] h-[40vh] bg-black flex flex-col justify-center items-center">
+        <h1 className="font-bold text-white text-4xl">Star Rating</h1>
+        <div className="flex mt-5 text-4xl">
+          {[1, 2, 3, 4, 5].map((value) => (
+            <div key={value}>
+              <input
+                type="radio"
+                onChange={() => handleChange(value)}
+                checked={value === checked}
+                className="hidden peer"
+                id={`star-${value}`}
+              />
+              <label htmlFor={`star-${value}`}>
+                <FontAwesomeIcon
+                  icon={faStar}
+                  className={`text-gray-300 ${
+                    checked >= value ? "text-yellow-400" : ""
+                  }`}
+                />
+              </label>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
